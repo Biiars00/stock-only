9b6ccbef2412f42629c8f91eae94f66291f40f2f
Biiars00 <biiaribeiro60@gmail.com>
2023-11-29 17:21:27 -0300

    refactor: delete file.

--- src/models/products.models.ts
+++
@@ -1,7 +0,0 @@
-import admin from 'firebase-admin';
-
-const database = admin.initializeApp({
-  credential: admin.credential.cert('src/database/produtosFirestore.json'),
-});
-
-export default database;
